4808b7b39695f6599ca9ff3b70cab8b655206c04
Shohruh003 <shohruhazimov003@gmail.com>
2022-06-15 18:02:45 +0500

    Add input-display

--- js/script.js
+++ js/script.js
@@ -24,6 +24,7 @@ elForm.addEventListener("submit", function(e) {
   } else {
     elInput.classList.remove("is-invalid");
     elInput.classList.add("is-valid");
+    elError.textContent = null;
   }
 
   function onFoot (num) {
